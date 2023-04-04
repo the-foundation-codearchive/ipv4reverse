4ed5648a97949be959e7e8949255902ae6bec7ff
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2023-04-04 05:58:41 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -6,8 +6,7 @@ octet_two=$2
 for octet_three in 0 $(seq 1 255);do 
 
 test -e lists/${octet_one}/${octet_one}.${octet_two} || mkdir -p lists/${octet_one}/${octet_one}.${octet_two}
-time (    ( echo {0..254}.${octet_three}.${octet_two}.${octet_one}.in-addr.arpa |sed 's/ /\n/g') | ( 
-            /tmp/dns -r /tmp/resolvers -t PTR -|grep PTR|grep -v PTR$) |while read a ;do echo $(date +%s)"|"$a ;done |tee lists/${octet_one}/${octet_one}.${octet_two}/${octet_one}.${octet_two}.${octet_three} |wc -l ) &
+time (     echo {0..254}.${octet_three}.${octet_two}.${octet_one}.in-addr.arpa |sed 's/ /\n/g' ) | /tmp/dns -r /tmp/resolvers -t PTR -|grep PTR|grep -v PTR$ |while read a ;do echo $(date +%s)"|"$a ;done |tee lists/${octet_one}/${octet_one}.${octet_two}/${octet_one}.${octet_two}.${octet_three} |wc -l ) &
 sleep 0.5
 wait
 done
