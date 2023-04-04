b10571295de2b987f73c9bdb30c186566dc28631
Benjamin Freitag <12718580+benchonaut@users.noreply.github.com>
2023-04-04 05:56:55 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,7 +7,7 @@ for octet_three in 0 $(seq 1 255);do
 
 test -e lists/${octet_one}/${octet_one}.${octet_two} || mkdir -p lists/${octet_one}/${octet_one}.${octet_two}
 time (    ( echo {0..254}.${octet_three}.${octet_two}.${octet_one}.in-addr.arpa |sed 's/ /\n/g') | ( 
-            /tmp/dns -r /tmp/resolvers -t PTR -|grep PTR|grep -v PTR$) |while read a ;do echo $(date+%s)"|"$a;done |tee lists/${octet_one}/${octet_one}.${octet_two}/${octet_one}.${octet_two}.${octet_three} |wc -l ) &
+            /tmp/dns -r /tmp/resolvers -t PTR -|grep PTR|grep -v PTR$) |while read a ;do echo $(date +%s)"|"$a ;done |tee lists/${octet_one}/${octet_one}.${octet_two}/${octet_one}.${octet_two}.${octet_three} |wc -l ) &
 sleep 0.5
 wait
 done
